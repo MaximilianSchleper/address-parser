c056adb7d6c038967e13e2115800abfdcd204854
Max Schleper <max.schlep@gmail.com>
2020-12-20 22:09:37 +0100

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+fs.createReadStream('addresses_no_duplicates.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    whole_street = row.wholestreet;
+    split_street = whole_street.split(/(\d+(.*))/);
+    console.log({'streetname': split_street[0], 'streetnumber': split_street[1]});
+  })
+  .on('end', () => {
+    console.log('done');
+  });
